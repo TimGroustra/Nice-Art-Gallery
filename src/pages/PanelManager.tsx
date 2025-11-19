ccc625e8fc2f434d17d97a32fa332b2e78ec4278
[dyad] <timothy.groustra@hotmail.com>
2025-11-19 12:25:37 +0200

    Reverted all changes back to version d9527823840932703e503c0d7168d87aa6314fd3

--- src/pages/PanelManager.tsx
+++
@@ -1,7 +0,0 @@
-import PanelLockManagerWrapper from '@/components/PanelLockManager';
-
-const PanelManagerPage = () => {
-  return <PanelLockManagerWrapper />;
-};
-
-export default PanelManagerPage;
