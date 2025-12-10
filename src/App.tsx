455593c6ef693feb568c03aa16d315d29138944f
[dyad] <timothy.groustra@hotmail.com>
2025-12-10 21:01:52 +0200

    Reverted all changes back to version 6a8c78a74d64259eca7717e5d4095519b7a0c371

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import GalleryConfig from "./pages/GalleryConfig";
 import Login from "./pages/Login";
-import CreateNft from "./pages/CreateNft"; // Import new page
 import { WagmiProvider } from 'wagmi';
 import { wagmiConfig } from '@/integrations/wagmi/config';
 
@@ -24,7 +23,6 @@ const App = () => (
             <Route path="/" element={<Index />} />
             <Route path="/login" element={<Login />} />
             <Route path="/gallery-config" element={<GalleryConfig />} />
-            <Route path="/create" element={<CreateNft />} /> {/* New route */}
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
