1d78d9922ff700ea0c086eacd493e4e00c80c137
[dyad] <timothy.groustra@hotmail.com>
2025-12-18 09:35:56 +0200

    [dyad] Fixing default export in Index.tsx - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,28 @@
+import React, { useState, useEffect } from 'react';
 import NftGallery from "@/components/NftGallery";
 import GalleryUI from "@/components/GalleryUI";
-// ... rest unchanged
+import { initializeGalleryConfig } from '@/config/galleryConfig';
+
+const Index: React.FC = () => {
+  const [instructionsVisible, setInstructionsVisible] = useState(true);
+
+  useEffect(() => {
+    initializeGalleryConfig();
+  }, []);
+
+  const handleLockClick = () => {
+    setInstructionsVisible(false);
+  };
+
+  return (
+    <div className="w-full h-screen relative">
+      <NftGallery setInstructionsVisible={setInstructionsVisible} />
+      <GalleryUI 
+        instructionsVisible={instructionsVisible} 
+        onLockClick={handleLockClick} 
+      />
+    </div>
+  );
+};
+
+export default Index;
